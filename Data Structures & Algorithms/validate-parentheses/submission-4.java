bf62069daf3beb14ca2a84a3b88ec246a1cff368
Jermaine <jermainet0325@gmail.com>
2026-05-21 22:45:46 -0400

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        Stack<Character> stack = new Stack();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        for(char c : s.toCharArray()){
+            if(map.containsKey(c)){
+                if(!stack.isEmpty() && stack.peek() == map.get(c)){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+                }
+                 else{
+                    stack.push(c);
+            } 
+        }
+        return stack.isEmpty();
+    }
+}
+
